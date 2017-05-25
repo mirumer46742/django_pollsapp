bcbf98f6ad5184aa66509fb472dac0030dc35027
root <root@mirumer-PC.(none)>
2017-05-25 19:02:08 +0530

    Added Generic Views

--- polls/urls.py
+++ polls/urls.py
@@ -2,14 +2,17 @@ from django.conf.urls import url
 
 from . import views
 
-app_name='polls'
+app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /polls/5/vote/
+    #polls
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    
+    #polls/172
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    
+    #polls/172/results
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    
+    #polls/172/vote
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,50 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
-from django.http import HttpResponseRedirect,HttpResponse
+#from django.http import HttpResponseRedirect,HttpResponse
+#from django.urls import reverse
+#from django.http import Http404
+#from django.shortcuts import render,get_object_or_404
+#from .models import Question, Choice
+
+
+#New imports for Generic Views#
+
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
-from django.http import Http404
+from .models import Choice, Question
 
-from django.shortcuts import render,get_object_or_404
-#from django.template import loader
 
-from .models import Question, Choice
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-#Create your views here
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    context = {'latest_question_list': latest_question_list}
-    return render(request,'polls/index.html',context)
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+
+#Code Changed due to generic Views #
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    context = {'latest_question_list': latest_question_list}
+#    return render(request,'polls/index.html',context)
+
+#Code Changed due to Render Function #
 #def index(request):
 #    latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #    template = loader.get_template('polls/index.html')
@@ -26,18 +53,23 @@ def index(request):
 #    }
 #    return HttpResponse(template.render(context, request))
 
-def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	#context = {'question': question}
-	return render(request, 'polls/detail.html',{'question': question})
+#Code Changed due to generic Views #
+#def detail(request, question_id):
+#	question = get_object_or_404(Question, pk=question_id)
+#	return render(request, 'polls/detail.html',{'question': question})
+
+
+#Code Changed due to get_object_or_404#
 #    try:
 #        question = Question.objects.get(pk=question_id)
 #    except Question.DoesNotExist:
 #        raise Http404("Question does not exist")
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+
+#Code Changed due to generic Views #
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
